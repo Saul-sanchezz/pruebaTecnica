325742f71d372554bc16c73fe9901e2323eb45cc
saul sanchez <saul_sanchezz@live.com>
2024-02-21 08:46:27 -0700

    modificacion de configuracion a base de datos

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 const option = {
-  autoIndex: false, // Don't build indexes  
-  // If not connected, return errors immediately rather than waiting for reconnect
-  //geting rid off the depreciation errors
+  autoIndex: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 }
@@ -20,28 +18,3 @@ const conectarDB = async () => {
 }
 
 module.exports = conectarDB
-
-
-
-
-// import mongoose from "mongoose";
-
-// const conectarDB = async () => {
-//   try {
-//     const connection = await mongoose.connect(
-//         process.env.MONGO_URI_LOCAL, {
-//           useNewUrlParser: true,
-//           useUnifiedTopology: true
-//         }
-//       )
-//       const url = `${connection.connection.host}:${connection.connection.port}`
-//       console.log(`mongoDB conectado en: ${url}`)
-//   } catch (error) {
-//    console.log(`error: ${error.message}`) 
-//    // es para forzar que el proceso termine
-//    process.exit(1)
-//   }
-// }
-
-// export default conectarDB
-
